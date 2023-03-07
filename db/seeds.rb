6de52bb4059c5a8b8e2765690fb6103b1096e645
Omari99 <mariam.ali01@student.moringaschool.com>
2023-03-07 11:12:07 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ puts "🌱 Seeding data..."
     price: rand(0..60) # random number between 0 and 60
   )
 
+
   # create between 1 and 5 reviews for each game
   rand(1..5).times do
     Review.create(
